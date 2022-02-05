d6d5f3b77c380f195b1d7373e7972d4226ca9194
knguyen438 <knguyen438@github.gatech.edu>
2022-02-05 11:11:46 -0500

    connect database

--- commerce/settings.py
+++ commerce/settings.py
@@ -6,10 +6,12 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+from dotenv import load_dotenv
 
 import os
 import django_heroku
- 
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
